0bea34ce5562587703009e6be81efe3ac8779aa8
loli <30765462+jinpogan@users.noreply.github.com>
2022-01-13 19:34:24 -0800

    Update steam账号管家无毒绿色版.py

--- steam账号管家无毒绿色版.py
+++ steam账号管家无毒绿色版.py
@@ -23,21 +23,27 @@ button_list = []
 root.iconbitmap(resource_path('image.ico'))
 root.title("steam账号管家无毒绿色版")
 root.resizable(False, False)
-try:
-    with codecs.open(os.getenv('APPDATA')+"\steam账号管家无毒绿色版.txt",encoding='utf-8') as f:
-        accounts=json.loads(f.read())
-        if accounts=={}:
-            print("1")
+if os.path.exists(os.getenv('APPDATA')+"\steam账号管家无毒绿色版.txt"):
+    while (1):
+        try:
+            with codecs.open(os.getenv('APPDATA')+"\steam账号管家无毒绿色版.txt",encoding='utf-8') as f:
+                accounts=json.loads(f.read())
+        except:
+            with codecs.open(os.getenv('APPDATA')+"\steam账号管家无毒绿色版.txt",encoding='utf-8') as f:
+                accounts=json.loads(f.read().encode().decode('utf-8-sig'))
+        if accounts=={} or accounts=={"<把这个删掉改成用户名>":"<把这个删掉改备注>","<把这个删掉改成用户名>":"<把这个删掉改备注>","<把这个删掉改成用户名>":"<把这个删掉改备注>"}:
             cmd("copy  accounts.txt %appdata%\steam账号管家无毒绿色版.txt  ")
             cmd("notepad %appdata%\steam账号管家无毒绿色版.txt")
-        counter=0
-        for account in accounts.keys():
-            button_list.append(tk.Button(root, text=accounts[account], command=lambda account=account: login(account)))
-            button_list[-1].grid(row=counter, column=0)
-            counter=counter+1
-        tk.Button(root, text="添加/删除账号", command=editaccs).grid(row=counter+2, column=0,pady= 20,)
-        root.mainloop()
-except Exception as e:
+        else:
+            break
+    counter=0
+    for account in accounts.keys():
+        button_list.append(tk.Button(root, text=accounts[account], command=lambda account=account: login(account)))
+        button_list[-1].grid(row=counter, column=0)
+        counter=counter+1
+    tk.Button(root, text="添加/删除账号", command=editaccs).grid(row=counter+2, column=0,pady= 20,)
+    root.mainloop()
+else:
     print(e)
     print("2")
     cmd("copy accounts.txt %appdata%\steam账号管家无毒绿色版.txt  ")
